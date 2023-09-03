92e3b47fb8e1fa3d525de5ac1df9b82e2a81c10c
Thriveni200309 <142702122+Thriveni200309@users.noreply.github.com>
2023-09-03 19:43:45 +0530

    Version-2
    Assignment

--- PatternPrograms/src/AssignmentPatternPrograms/PatternToPrintM.java
+++ PatternPrograms/src/AssignmentPatternPrograms/PatternToPrintM.java
@@ -5,7 +5,7 @@ public class PatternToPrintM {
 		int n=10;
 		for(int i=0;i<=n;i++) {
 			for(int j=0;j<=n;j++) {
-				if(j==0||j==n||i<=n/2&&i==j)
+				if(j==0||j==n||j==n-i&&i<=n/2||i==j&&j<=n/2)
 					System.out.print("*");
 				else
 					System.out.print(" ");
